ffba46aee5331a37e4dc0cf6997089a588da16c7
Eric Boittier <40135422+EricBoittier@users.noreply.github.com>
2021-03-16 16:11:21 +0100

    Update Optimize.py

--- Optimize.py
+++ Optimize.py
@@ -3,6 +3,7 @@ import pickle
 from MDCMgrid import *
 import sys
 
+
 class out():
     def __init__(self, x=None, from_pickle=None):
         if from_pickle:
@@ -28,21 +29,21 @@ def optimize_and_pickle(mdcm, out_pickle, out, eps=None, maxiter=None):
     print(out)
     pickle.dump(out, open("{}".format(out_pickle), "wb"))
 
+
 if __name__ == '__main__':
     charges_path = sys.argv[1]
     pcube = sys.argv[2]
     in_pickle = sys.argv[3]
     out_pickle = sys.argv[4]
-    
+
     eps = None
     maxiter = None
     if len(sys.argv) > 5:
-    	eps=sys.argv[5]
+        eps = sys.argv[5]
     if len(sys.argv) > 6:
-	maxiter=sys.argv[6]   
-    
+        maxiter = sys.argv[6]
+
     out = out(from_pickle="out.p")
     mdcm = MDCM_cube_comparison(charges_path, pcube)
 
     optimize_and_pickle(mdcm, out_pickle, out, eps=eps, maxiter=maxiter)
-
